d446e5d2218fe83cb1b64e30b2379c0ae8ae9b7b
mathieu stamm <mathieu.stamm@gmail.com>
2024-11-04 18:46:58 +0100

    add headlessui and heroicons

--- tailwind.config.mjs
+++ tailwind.config.mjs
@@ -2,12 +2,21 @@ import flowbite from "flowbite-react/tailwind";
 
 /** @type {import('tailwindcss').Config} */
 export default {
-  content: [
-    "./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}",
-    flowbite.content(),
-  ],
-  theme: {
-    extend: {},
-  },
-  plugins: [flowbite.plugin()],
+    content: [
+        "./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}",
+        flowbite.content(),
+    ],
+    theme: {
+        extend: {
+            colors: {
+                background: "var(--background)",
+                foreground: "var(--foreground)",
+            },
+            width: {
+                "3/10": "30%",
+                "7/10": "70%",
+            },
+        },
+    },
+    plugins: [flowbite.plugin()],
 };
